f243a6d0b9b2cb750180f7661303cbdb57925351
JohnBaker1 <jbaker@student.sjcny.edu>
2017-02-04 02:54:37 -0500

    Src Folder

---
+++ src/main/java/johnbaker/johnbakerproj1/VRPC.java
@@ -0,0 +1,13 @@
+package johnbaker.johnbakerproj1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VRPC extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vrpc);
+    }
+}
